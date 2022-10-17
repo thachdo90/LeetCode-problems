7e203b40790334f501b4444f45d2c583257d34af
Thach Do <thachdo90@gmail.com>
2022-10-17 00:16:49 -0700

    fixed delete duplicate email

--- SQL/196DeleteDuplicateEmail.sql
+++ SQL/196DeleteDuplicateEmail.sql
@@ -1,4 +1,14 @@
+-- can't update from the same table
+-- this doesn't work
+-- DELETE FROM Person
+-- WHERE id NOT IN
+-- (SELECT MIN(id) id FROM PERSON
+-- GROUP BY email) as t2
+
+
+-- but this works if we wrap it in another select, is this like making a copy of that result?
 DELETE FROM Person
 WHERE id NOT IN
-(SELECT MIN(id) FROM PERSON
-GROUP BY email)
+(SELECT id FROM
+(SELECT MIN(id) id FROM PERSON
+GROUP BY email) as t2)
