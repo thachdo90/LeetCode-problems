5b7dba2b2be826f9f59e9201dc6f068ead26a7e8
Thach Do <thachdo90@gmail.com>
2022-10-26 20:05:02 -0700

    tried optimizing brute force but it didn't work

--- hashmaps/16ThreeSumClosest.js
+++ hashmaps/16ThreeSumClosest.js
@@ -16,3 +16,27 @@ var threeSumClosest = function(nums, target) {
   }
   return closestSum;
 };
+
+// tried with optimization but still only beating 5%
+var threeSumClosest = function(nums, target) {
+  nums.sort((a, b) => a - b);
+  let closestSum = nums[0] + nums[1] + nums[2];
+  let diff = Math.abs(closestSum - target)
+  for (let pointer1 = 0; pointer1 < nums.length - 2; pointer1++) {
+    let oneSumDiff = Math.abs(target - nums[pointer1]);
+    for (let pointer2 = pointer1 + 1; pointer2 < nums.length - 1; pointer2++) {
+      if (nums[pointer2] - oneSumDiff > diff) break;
+      let twoSumDiff = Math.abs(target - nums[pointer1] - nums[pointer2]);
+      for (let pointer3 = pointer2 + 1; pointer3 < nums.length; pointer3++) {
+        if (nums[pointer3] - twoSumDiff > diff) break;
+        let currentSum = nums[pointer1] + nums[pointer2] + nums[pointer3];
+        let newDiff = Math.abs(currentSum - target);
+        if (newDiff < diff) {
+          closestSum = currentSum;
+          diff = newDiff;
+        }
+      }
+    }
+  }
+  return closestSum;
+};
