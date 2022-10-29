5c986d20e75222d7a21f3e5e47778c2676cf552d
Thach Do <thachdo90@gmail.com>
2022-10-28 18:37:14 -0700

    implemented list helpers

---
+++ linkedLists/listHelpers.js
@@ -0,0 +1,21 @@
+function ListNode(val, next) {
+  this.val = (val===undefined ? 0 : val)
+  this.next = (next===undefined ? null : next)
+}
+
+const generateNodeFromArray = (array) => {
+  let previousNode = null;
+  while (array.length > 0) {
+      let node = new ListNode(array.pop())
+      node.next = previousNode;
+      previousNode = node;
+  }
+  return previousNode;
+}
+
+const printList = (head) => {
+  while (head) {
+  console.log(head.val)
+  head = head.next;
+  }
+}
