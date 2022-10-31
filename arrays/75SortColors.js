250b56735ec1245564e9aca63f7984ccbdc93853
Thach Do <thachdo90@gmail.com>
2022-10-31 14:35:24 -0700

    solved sort colors with Dijkstra's algo

--- arrays/75SortColors.js
+++ arrays/75SortColors.js
@@ -37,3 +37,34 @@ var sortColors = function(nums) {
   }
   return nums;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+//  I: array of ints
+// O: same array
+// C: no native sorts, in-place only
+// E: only one or two colors presents, array length is 1
+
+// Strategy: one pass with Dijkstra's algorithm
+// big idea: move all the 0's to the front, move all the 2's back, what's left is the 1's in the middle
+// init p0 and pointer = 0, p2 at the first non-2 from the right
+// move pointer up, if it's 0, swap it with p0
+// if it's a 2, swap it with p2
+// if it's a 1, do nothing
+var sortColors = function(nums) {
+  let p0 = -1;
+  let pointer = 0;
+  let p2 = nums.length;
+  while (pointer < p2) {
+    if (nums[pointer] === 0) {
+      p0++;
+      [nums[pointer], nums[p0]] = [nums[p0], nums[pointer]];
+    } else if (nums[pointer] === 2) {
+      while ((nums[p2] === 2 || nums[p2] === undefined) && p2 > pointer) p2--;
+      [nums[pointer], nums[p2]] = [nums[p2], nums[pointer]];
+    }
+    if (p0 === pointer || nums[pointer] === 1) pointer++;
+  }
+};
