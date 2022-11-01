00e34522b64a5a493b51fd76cd7c76077fcb0323
Thach Do <thachdo90@gmail.com>
2022-11-01 11:02:18 -0700

    optimized search 2d matrix with divide and conquer strategy

--- arrays/240Seach2DMatrix2.js
+++ arrays/240Seach2DMatrix2.js
@@ -43,3 +43,117 @@ var searchMatrix = function(matrix, target) {
   }
   return false;
 };
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+// I: matrix
+// O: boolean
+// C:
+// E: not found, value at 0,0 or at one of the corners
+
+// observation:
+// bottom right is biggest, top left is smallest, use this to rule out some edge cases
+// top left corner will always be the min for that sub matrix
+
+// strategy: iterate through each row and binary search each row
+var searchMatrix = function(matrix, target) {
+  const binarySearchRow = (row) => {
+    let LB = 0;
+    let UB = matrix[row].length - 1;
+    if (matrix[row][LB] === target || matrix[row][UB] === target) return true;
+    while (LB < UB - 1) {
+      let midIndex = Math.floor((LB + UB) / 2);
+      let midValue = matrix[row][midIndex];
+      if (midValue < target) {
+        LB = midIndex;
+      } else if (midValue > target) {
+        UB = midIndex;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  for (let row = 0; row < matrix.length; row++) {
+    let rowMin = matrix[row][0];
+    let rowMax = matrix[row][matrix[row].length - 1];
+    if (rowMin <= target && target <= rowMax) {
+      if (binarySearchRow(row)) return true;
+    } else if (target > rowMax) {
+      continue;
+    } else if (target < rowMin) {
+      break;
+    }
+  }
+  return false;
+};
+
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+// I: matrix
+// O: boolean
+// C:
+// E: not found, value at 0,0 or at one of the corners
+
+// observation:
+// bottom right is biggest, top left is smallest, use this to rule out some edge cases
+// top left corner will always be the min for that sub matrix
+
+// strategy: iterate through each row
+// optimization: binary search each row
+// optimization: divide and conquer, use the the major diagonal to find the two sub matrices that could contain the target
+var searchMatrix = function(matrix, target) {
+  if (target < matrix[0][0] || target > matrix[matrix.length - 1][matrix[0].length - 1]) return false;
+
+  const binarySearchRow = (row, c1, c2) => {
+    let LB = c1;
+    let UB = c2;
+    if (matrix[row][LB] === target || matrix[row][UB] === target) return true;
+    while (LB < UB - 1) {
+      let midIndex = Math.floor((LB + UB) / 2);
+      let midValue = matrix[row][midIndex];
+      if (midValue < target) {
+        LB = midIndex;
+      } else if (midValue > target) {
+        UB = midIndex;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  const searchSubMatrix = (r1, c1, r2, c2) => {
+    for (let row = r1; row <= r2; row++) {
+      let rowMin = matrix[row][c1];
+      let rowMax = matrix[row][c2];
+      if (rowMin <= target && target <= rowMax) {
+        if (binarySearchRow(row, c1, c2)) return true;
+      } else if (target > rowMax) {
+        continue;
+      } else if (target < rowMin) {
+        break;
+      }
+    }
+    return false;
+  }
+
+  let row = 0;
+  let col = 0;
+  while (matrix[row][col] <= target) {
+    if (matrix[row][col] === target) return true;
+    if (row === matrix.length - 1) return searchSubMatrix(0, col + 1, row, matrix[row].length - 1);
+    if (col === matrix[0].length - 1) return searchSubMatrix(row + 1, 0, matrix.length - 1, col);
+    if (matrix[row + 1][col + 1] > target) return searchSubMatrix(0, col + 1, row, matrix[row].length - 1) || searchSubMatrix(row + 1, 0, matrix.length - 1, col);
+    row++;
+    col++;
+  }
+
+};
