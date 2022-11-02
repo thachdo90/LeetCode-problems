1dccd21112624e6b90566bee3c158458aa156666
Thach Do <thachdo90@gmail.com>
2022-11-02 15:43:38 -0700

    solved product of array except self using hashmap

--- arrays/238ProductOfArrayExceptSelf.js
+++ arrays/238ProductOfArrayExceptSelf.js
@@ -9,3 +9,41 @@ var productExceptSelf = function(nums) {
   }
   return results;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+// I: array of ints
+// O: array
+// C: O(n) time, stretch O(1) space, not division operation
+// E: 0, negative numbers
+
+// if we could use division, we would take the product of the array and divide by each element
+// observation, we need to iterate through all in order to get the product
+// without division, we have to find some way to exclude a number as the product is built
+
+// we could do this in quadratic time, for each element, we would multiple everything except itself
+
+// iterate first pass, get product of array
+// iterate second pass,
+var productExceptSelf = function(nums) {
+  let leftProduct = {'-1': 1};
+  let rightProduct = {[nums.length]: 1};
+  let product = 1;
+  for (let left = 0; left <= nums.length - 1; left++) {
+    product *= nums[left];
+    leftProduct[left] = product;
+  }
+  product = 1;
+  for (let right = nums.length - 1; right >= 1; right--) {
+    product *= nums[right];
+    rightProduct[right] = product;
+  }
+  let results = [];
+  for (let i = 0; i < nums.length; i++) {
+    let product = leftProduct[i - 1] * rightProduct[i + 1];
+    results.push(product);
+  }
+  return results;
+};
