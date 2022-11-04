2db016687c41f33631cbd9e9460c4fc9e4e54694
Thach Do <thachdo90@gmail.com>
2022-11-03 19:33:14 -0700

    optimized with two passes, going right, going left

--- strings/38LongestValidParens.js
+++ strings/38LongestValidParens.js
@@ -42,3 +42,61 @@ var longestValidParentheses = function(s) {
   return longestCount;
 
 };
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+// I: String
+// O: number
+// C:
+// E: string length of 0
+
+// Observation: we could iterate and match up the number of open to closed parentheses to see if it's valid, but this will miss the nested valid substrings
+
+// Strategy: one pass O(n)
+// if the number of closed parentheses exceed open, we'll restart the count
+var longestValidParentheses = function(s) {
+  let longestCount = 0;
+  let openCount = 0;
+  let count = 0;
+  for (let char of s) {
+    if (char === '(') {
+      openCount++;
+      count++;
+    } else {
+      if (openCount > 0) {
+        count++;
+        openCount--;
+        if (openCount === 0) longestCount = Math.max(longestCount, count);
+      } else {
+        // reset counts, starting new substring
+        longestCount = Math.max(longestCount, count);
+        openCount = 0;
+        count = 0;
+      }
+    }
+  }
+  openCount = 0;
+  count = 0;
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === ')') {
+      openCount++;
+      count++;
+    } else {
+      if (openCount > 0) {
+        count++;
+        openCount--;
+        if (openCount === 0) longestCount = Math.max(longestCount, count);
+      } else {
+        // reset counts, starting new substring
+        longestCount = Math.max(longestCount, count);
+        openCount = 0;
+        count = 0;
+      }
+    }
+  }
+
+  return longestCount;
+
+};
