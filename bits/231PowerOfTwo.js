c1b8080ce1ad76aede3613fc26ab2cf3eab8c868
Thach Do <thachdo90@gmail.com>
2022-11-09 21:00:24 -0800

    solved power of two

---
+++ bits/231PowerOfTwo.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+ var isPowerOfTwo = function(n) {
+
+  while (n > 1) {
+    if (n % 2 !== 0) {
+      return false;
+    } else {
+      n = n / 2;
+    }
+  }
+  return n === 1;
+
+};
