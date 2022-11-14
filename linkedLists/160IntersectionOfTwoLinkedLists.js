017ed886706430a0d0dfb3aa15dc3fdea2a45c29
Thach Do <thachdo90@gmail.com>
2022-11-13 20:45:37 -0800

    solved intersetion of linked list by using pointers and two passes to measure length

--- linkedLists/160IntersectionOfTwoLinkedLists.js
+++ linkedLists/160IntersectionOfTwoLinkedLists.js
@@ -25,3 +25,39 @@ var getIntersectionNode = function(headA, headB) {
   return null
 
 };
+
+var getIntersectionNode = function(headA, headB) {
+  let lengthA = 1;
+  let lengthB = 1;
+  let pointerA = headA;
+  while (pointerA.next) {
+    pointerA = pointerA.next;
+    lengthA++;
+  }
+  let pointerB = headB;
+  while (pointerB.next) {
+    pointerB = pointerB.next;
+    lengthB++;
+  }
+  pointerA = headA;
+  pointerB = headB;
+  if (lengthA < lengthB) {
+    for (let count = 1; count <= lengthB - lengthA; count++) {
+      pointerB = pointerB.next;
+    }
+  } else if (lengthA > lengthB) {
+    for (let count = 1; count <= lengthA - lengthB; count++) {
+      pointerA = pointerA.next;
+    }
+  }
+  while (pointerA) {
+    if (pointerA === pointerB) {
+      return pointerA;
+    } else {
+      pointerA = pointerA.next;
+      pointerB = pointerB.next;
+    }
+  }
+  return null;
+
+};
