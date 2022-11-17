dc68d2f8b47a4885b7a8d8dc29f8a739487b6259
Thach Do <thachdo90@gmail.com>
2022-11-16 17:02:38 -0800

    refactored so that the function iterates through the tree only when next is being called rather than generating the entire array at once

--- trees/173BSTIterator.js
+++ trees/173BSTIterator.js
@@ -47,3 +47,51 @@ BSTIterator.prototype.hasNext = function() {
  * var param_1 = obj.next()
  * var param_2 = obj.hasNext()
  */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ */
+ var BSTIterator = function(root) {
+  this.currentVal = Number.NEGATIVE_INFINITY;
+  this.stack = [];
+  while (root) {
+    this.stack.push(root);
+    root = root.left;
+  }
+};
+
+/**
+ * @return {number}
+ */
+BSTIterator.prototype.next = function() {
+  let node = this.stack.pop();
+  this.currentVal = node.val;
+  node = node.right;
+  while (node) {
+    this.stack.push(node);
+    node = node.left;
+  }
+  return this.currentVal;
+};
+
+/**
+ * @return {boolean}
+ */
+BSTIterator.prototype.hasNext = function() {
+  return this.stack.length > 0;
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * var obj = new BSTIterator(root)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
