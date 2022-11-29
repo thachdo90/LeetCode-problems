bf287268dc2ced774ad1d7ebad69941397719183
Thach Do <thachdo90@gmail.com>
2022-11-29 11:46:54 -0800

    optimized 713 using sliding window and math

--- arrays/713SubarrayProductLessThanK.js
+++ arrays/713SubarrayProductLessThanK.js
@@ -25,3 +25,46 @@ var numSubarrayProductLessThanK = function(nums, k) {
   }
   return results;
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+
+// I: array of numbers
+// O: number (a count of the subarrays that satisfies condition)
+// C: all numbers are greater than or equal to 1
+// E:
+// strategy: using math and a sliding window
+// find the first largest possible window that multiplies to be less than k
+// whenever k
+// challenge not double counting subarrays
+var numSubarrayProductLessThanK = function(nums, k) {
+  let result = 0;
+  let left = 0;
+  let right = 0;
+  let product = 1;
+  let previousEndIndex = -1;
+  const numOfSubArrays = (count) => {
+    return (1 + count) / 2 * count;
+  }
+  while (right <= nums.length) {
+    product *= nums[right] || 1;
+    if (product >= k || right === nums.length) {
+      let count = right - left;
+      result += numOfSubArrays(count);
+      if (left <= previousEndIndex) {
+        let overlap = numOfSubArrays(previousEndIndex - left + 1);
+        result -= overlap;
+      }
+      previousEndIndex = right - 1;
+      while (product >= k) {
+        product /= nums[left];
+        left++;
+      }
+    }
+    right++;
+  }
+  return result;
+};
